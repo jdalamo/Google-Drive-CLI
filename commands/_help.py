1a22f79c736466758e65381865e70a659453b28a
jdalamo <alamojad@gmail.com>
2019-09-04 19:49:11 -0500

    updated help

--- commands/_help.py
+++ commands/_help.py
@@ -4,7 +4,7 @@ def run(params):
 	param = ' '.join(params)
 	if len(params) == 0:
 		helpOutput = """
-This is a command line interface for Google Drive.  You can use the same bash commands you're used to in order navigate and view your files and folders.  Run the help command followed by the name of another command to get more information on how to use it.
+This is a command line interface for Google Drive. You can use the same bash commands you're used to in order navigate and view your files and folders. Run the help command followed by the name of another command to get more information on how to use it.
 List of commands:
 pwd
 ls
