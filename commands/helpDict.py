dc345a80a010dbaf20dfd6a3531419292855cb99
jdalamo <alamojad@gmail.com>
2019-09-04 19:48:12 -0500

    updated help text

--- commands/helpDict.py
+++ commands/helpDict.py
@@ -1,10 +1,14 @@
 helpText = {
     'pwd': "Type 'pwd' to output the current working directory.",
-    'ls': """Type 'ls' to output the contents of the current working directory.
-Another directory may be passed in as a parameter.""",
+    'ls': "Type 'ls' to output the contents of the current working directory. Another directory may be passed in as a parameter.",
     'cd': "Type 'cd' followed by the name of a directory to move into that directory.",
+    'clear': "Type 'clear' to clear the terminal.",
     'quit': "Type 'quit' to quit the application.",
-    'sync up': "Type 'sync up' to push all locally created files for tracked folders to Google Drive.",
-    'sync down': "Type 'sync down' to pull all remotely created files for tracked folders from Google Drive.",
-    'dl': "Type 'dl' and then the name of a file in the current directory to download that file."
+    'sync up': "Type 'sync up' to push all locally created files for tracked folders to their respective Google Drive folders. Tracked folders allow you to keep a local folder and a Google Drive folder in sync.  Type 'help tf' for more information about tracked folders.  Support for pulling/pushing changes to pre-existing files is coming.",
+    'sync down': "Type 'sync down' to pull all files in tracked folders from Google Drive to the local folder set to track it.  Tracked folders allow you to keep a local folder and a Google Drive folder in sync.  Type 'help tf' to get more information about tracked folders.  Support for pulling/pushing changes to pre-existing files is coming.",
+    'dl': "Type 'dl' followed by the name of a file in the current directory to download that file.",
+    'tf': "Tracked folders allow you to easily keep local folders and Google Drive folders in sync. If you pair a local folder with a Google Drive folder with the 'tf add' command, you can then use 'sync up' and 'sync down' to automatically add newly created files in one location to the other.",
+    'tf view': "Type 'tf view' to view all currently tracked folder pairs.",
+    'tf add': "Type 'tf add' to add a tracked folder pair. After running the command, you will be prompted to enter the name of the Google Drive folder (must be in the working directory) and the full path to the local folder.",
+    'tf del': "Type 'tf del' followed by the name of a tracked folder pair, which can be seen with the 'tf view' command, to stop those folders from tracking."
 }
